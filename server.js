0ade415807508ff8444b685857ac90d6eba3a2d8
Sebastian Grant <sebastianlgrant@gmail.com>
2023-05-13 16:49:20 +1000

    fixing server

--- server.js
+++ server.js
@@ -77,17 +77,19 @@ app.use('/api/report', require('./routes/api/reportRouter'));
 
 // app.use(globalErrorHandler);
 
-// // ... other imports 
-// const path = require("path")
+// ... other imports 
+const path = require("path")
 
-// // ... other app.use middleware 
-// app.use(express.static(path.join(__dirname, "client", "build")))
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-// // ...
-// // Right before your app.listen(), add this:
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'test') {
   server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
